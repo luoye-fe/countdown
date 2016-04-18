c681e33637e6af797caa2c29cbfc19432a80d8d1
luoye <842891024@qq.com>
2016-04-18 15:24:41 +0800

    complete

--- app.js
+++ app.js
@@ -1,32 +1,59 @@
 
+var htmlToDate = function(inputs){
+	var result = '';
+	var splitSign = ['-','-',' ',':',':',':',''];
+	for(var i = 0;i < inputs.length;i++){
+		if(inputs[i].value !== ''){
+			result += inputs[i].value + splitSign[i];
+		}
+	}
+	return result;
+}
 
-// var countdown1 = CountDown.init({
-// 	ele: ele1,
-//     startTime: Date.now() - 7 * 24 * 60 * 60 * 1000,
-//     endTime: Date.now(),
-// })
+var htmlToUnit = function(inputs){
+	var unit = {};
+	for(var i = 0;i < inputs.length;i++){
+		unit[inputs[i].id] = inputs[i].checked;
+	}
+	return unit;
+}
 
+var countdown;
 
+var init = function(){
+	var options = {
+		onStart: function() {
+			console.log('start!');
+		},
+		onChange: function(value) {
+			// console.log(value)
+		},
+		onStop: function() {
+			console.log('stop!');
+		}
+	};
 
-var countdown2 = CountDown.init({
-    startTime: '2016-01-01 19:39:11:666',
-    endTime: '2016-04-17 23:59:00:666',
-    unit: {
-    	year: false,
-    	month: false,
-    	day: false,
-    	hour: false,
-    	minute: false,
-    	second: true
-    },
-    onStart: function() {
+	// 解析开始结束时间
+	var startInputs = document.querySelectorAll('.input_con')[0].querySelectorAll('input');
+	options.startTime = htmlToDate(startInputs);
+	var endInputs = document.querySelectorAll('.input_con')[1].querySelectorAll('input');
+	htmlToDate(endInputs) === '' ? null : options.endTime = htmlToDate(endInputs);
 
-    },
-    onChange: function(value) {
-    	// console.log(111)
-    },
-    onEnd: function() {
+	var unitInputs = document.querySelectorAll('.input_con')[2].querySelectorAll('input');
+	options.unit = htmlToUnit(unitInputs);
 
-    }
-})
+	try{
+		countdown.stop();
+	}catch(e){};
+
+	countdown = CountDown.init(options);
+
+	countdown.change = function(value){
+		console.log(value);
+		document.querySelectorAll('.timer')[0].innerHTML = (value.day ? value.day + '天' : '') + (value.hour ? value.hour + '时' : '') +(value.minute ? value.minute + '分' : '') +(value.second ? value.second + '秒' : '');
+		document.querySelectorAll('.code')[0].innerHTML = '参数：' + JSON.stringify(options);
+		document.querySelectorAll('.code')[1].innerHTML = '结果：' + JSON.stringify(value);
+	}
+
+}
 
--- countdown.js
+++ countdown.js
@@ -2,10 +2,6 @@
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : global.CountDown = factory();
 })(this, function() {
 
-    var _warn = function(value) {
-        console.warn(value);
-    }
-
     var _erro = function(err) {
         throw new Error(err);
     }
@@ -19,25 +15,37 @@
         return source;
     }
 
-    var _daysPerMonth = function() {
-
-    }
-
-    var _daysPerYear = function() {
-
-    }
-
-
     var _startMs = _endMs = null;
     var _diff;
 
+    var _result = {};
+    var timeMsMap = {
+        day: 24 * 60 * 60 * 1000,
+        hour: 60 * 60 * 1000,
+        minute: 60 * 1000,
+        second: 1000
+    }
 
     var _formatTimeByUnit = function(sMs, eMs, unit) {
-        var diff = Math.abs(sMs - eMs);
-        // console.log(diff);
-        var ts;
-    console.log(new Date(diff).getMonth());
-        console.log(Math.floor(diff / (24 * 60 * 60 * 1000)));
+        _result = {};
+        var hasGone = 0;
+        if (unit.day) {
+            _result.day = Math.floor((eMs - sMs - hasGone) / timeMsMap.day);
+            hasGone += _result.day * timeMsMap.day
+        }
+        if (unit.hour) {
+            _result.hour = Math.floor((eMs - sMs - hasGone) / timeMsMap.hour);
+            hasGone += _result.hour * timeMsMap.hour
+        }
+        if (unit.minute) {
+            _result.minute = Math.floor((eMs - sMs - hasGone) / timeMsMap.minute);
+            hasGone += _result.minute * timeMsMap.minute
+        }
+        if (unit.second) {
+            _result.second = Math.floor((eMs - sMs - hasGone) / timeMsMap.second);
+            hasGone += _result.second * timeMsMap.second
+        }
+        return _result;
     }
 
     var countDown = function(params) {
@@ -46,28 +54,24 @@
         this.params = _mixin(this.params, params);
         this.params.startTime = this.params.startTime ? new Date(this.params.startTime) : Date.now();
         this.params.endTime = this.params.endTime ? new Date(this.params.endTime) : null;
-        this.params.unit = this.params.unit ? _mixin(this.params.unit, {
-            year: false,
-            month: false,
+        this.params.unit = this.params.unit ? _mixin({
             day: false,
             hour: false,
             minute: false,
             second: true
-        }) : {
-            year: false,
-            month: false,
+        }, this.params.unit) : {
             day: false,
             hour: false,
             minute: false,
             second: true
         };
-        console.log(this.params);
+
         if (this.params.startTime.toString() === 'Invalid Date' || this.params.endTime === null ? 0 : this.params.endTime.toString() === 'Invalid Date') {
             _erro('Invalid Date in parmas!');
         }
         _startMs = Date.parse(this.params.startTime);
-        _endMs = this.params.endTime === null ? Number.POSITIVE_INFINITY : Date.parse(this.params.endTime);
-        // _diff = _endMs === Number.POSITIVE_INFINITY ? Number.POSITIVE_INFINITY : Math.abs(_endMs - _startMs);
+        _diff = this.params.endTime === null ? 1000 : -1000;
+        _endMs = this.params.endTime === null ? Date.now() : Date.parse(this.params.endTime);
         this.running = false;
         this.paste = false;
     }
@@ -76,7 +80,6 @@
         var _this = this;
         _this.running = true;
         _this.start && _this.start();
-        _this.params.onStart && _this.params.onStart();
         var timeOut = function() {
             if (!_this.running) {
                 return;
@@ -88,18 +91,24 @@
                         timeOut();
                     }
                 }, 50);
+                return;
             } else {
+
                 var _s = Date.now();
-                _endMs = _endMs === Number.POSITIVE_INFINITY ? _s : _endMs;
-                _formatTimeByUnit(_startMs, _endMs, _this.params.unit);
-                _endMs = _endMs === Number.POSITIVE_INFINITY ? _endMs + 1000 : _endMs - 1000;
+                var value = _formatTimeByUnit(_startMs, _endMs, _this.params.unit);
+                _endMs = _endMs + _diff;
                 var _e = Date.now();
-                var diff = _e - _s;
+                var diffPerFunc = _e - _s;
                 var timer = window.setTimeout(function() {
-                    _this.change && _this.change();
-                    _this.params.onChange && _this.params.onChange();
-                    timeOut();
-                }, 1000 - diff);
+                    if (_endMs <= _startMs) {
+                        _this.stop && _this.stop();
+                        clearTimeout(timer);
+                        return;
+                    } else {
+                        _this.change && _this.change(value);
+                        timeOut();
+                    }
+                }, 1000 - diffPerFunc);
             }
         }
         timeOut();
@@ -107,14 +116,16 @@
     countDown.prototype.toggle = function() {
         this.paste = !this.paste;
     }
-
+    countDown.prototype.start = function() {
+        this.params.onStart && this.params.onStart();
+    }
+    countDown.prototype.change = function(value) {
+        this.params.onChange && this.params.onChange(value);
+    }
     countDown.prototype.stop = function() {
         this.running = this.paste = false;
+        this.params.onStop && this.params.onStop();
     }
-
-
-    countDown.prototype.formatTime = _formatTimeByUnit;
-
     return {
         init: function(params) {
             var c = new countDown(params);
